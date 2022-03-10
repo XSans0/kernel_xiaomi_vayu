932304173043cc93869881efce17cbd951749bf1
Pranav Vashi <neobuddy89@gmail.com>
2022-03-10 22:49:21 +0530

    scsi: ufs: Clean up and fix clang compilation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: XSans0 <xsansdroid@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -53,12 +53,14 @@
 #include "ufs-debugfs.h"
 #include "ufs-qcom.h"
 
+#if 0
 static bool ufshcd_wb_sup(struct ufs_hba *hba);
 static int ufshcd_wb_ctrl(struct ufs_hba *hba, bool enable);
 static int ufshcd_wb_buf_flush_enable(struct ufs_hba *hba);
 static int ufshcd_wb_buf_flush_disable(struct ufs_hba *hba);
 static bool ufshcd_wb_is_buf_flush_needed(struct ufs_hba *hba);
 static int ufshcd_wb_toggle_flush_during_h8(struct ufs_hba *hba, bool set);
+#endif
 
 #ifdef CONFIG_DEBUG_FS
 
@@ -369,12 +371,7 @@ ufs_get_pm_lvl_to_link_pwr_state(enum ufs_pm_level lvl)
 	return ufs_pm_lvl_states[lvl].link_state;
 }
 
-#if 1
-static inline void ufshcd_wb_toggle_flush(struct ufs_hba *hba)
-{
-	return;
-}
-#else
+#if 0
 static inline void ufshcd_wb_toggle_flush(struct ufs_hba *hba)
 {
 	/*
@@ -390,12 +387,7 @@ static inline void ufshcd_wb_toggle_flush(struct ufs_hba *hba)
 }
 #endif
 
-#if 1
-static inline void ufshcd_wb_config(struct ufs_hba *hba)
-{
-	return;
-}
-#else
+#if 0
 static inline void ufshcd_wb_config(struct ufs_hba *hba)
 {
 	int ret;
@@ -1914,7 +1906,9 @@ static int ufshcd_devfreq_scale(struct ufs_hba *hba, bool scale_up)
 
 	/* Enable Write Booster if we have scaled up else disable it */
 	up_write(&hba->lock);
+#if 0
 	ufshcd_wb_ctrl(hba, scale_up);
+#endif
 	down_write(&hba->lock);
 	goto clk_scaling_unprepare;
 
@@ -7017,12 +7011,7 @@ out:
 				__func__, err);
 }
 
-#if 1
-static bool ufshcd_wb_sup(struct ufs_hba *hba)
-{
-	return false;
-}
-#else
+#if 0
 static bool ufshcd_wb_sup(struct ufs_hba *hba)
 {
 #if defined(CONFIG_UFSTW)
@@ -7040,12 +7029,7 @@ static bool ufshcd_wb_sup(struct ufs_hba *hba)
 }
 #endif
 
-#if 1
-static int ufshcd_wb_ctrl(struct ufs_hba *hba, bool enable)
-{
-	return 1;
-}
-#else
+#if 0
 static int ufshcd_wb_ctrl(struct ufs_hba *hba, bool enable)
 {
 	int ret;
@@ -7075,12 +7059,7 @@ static int ufshcd_wb_ctrl(struct ufs_hba *hba, bool enable)
 }
 #endif
 
-#if 1
-static int ufshcd_wb_toggle_flush_during_h8(struct ufs_hba *hba, bool set)
-{
-	return 1;
-}
-#else
+#if 0
 static int ufshcd_wb_toggle_flush_during_h8(struct ufs_hba *hba, bool set)
 {
 	int val, ret;
@@ -7103,12 +7082,7 @@ static int ufshcd_wb_toggle_flush_during_h8(struct ufs_hba *hba, bool set)
 }
 #endif
 
-#if 1
-static int ufshcd_wb_buf_flush_enable(struct ufs_hba *hba)
-{
-	return 0;
-}
-#else
+#if 0
 static int ufshcd_wb_buf_flush_enable(struct ufs_hba *hba)
 {
 	int ret;
@@ -7127,12 +7101,7 @@ static int ufshcd_wb_buf_flush_enable(struct ufs_hba *hba)
 }
 #endif
 
-#if 1
-static int ufshcd_wb_buf_flush_disable(struct ufs_hba *hba)
-{
-	return 0;
-}
-#else
+#if 0
 static int ufshcd_wb_buf_flush_disable(struct ufs_hba *hba)
 {
 	int ret;
@@ -7151,12 +7120,7 @@ static int ufshcd_wb_buf_flush_disable(struct ufs_hba *hba)
 }
 #endif
 
-#if 1
-static bool ufshcd_wb_is_buf_flush_needed(struct ufs_hba *hba)
-{
-	return false;
-}
-#else
+#if 0
 static bool ufshcd_wb_is_buf_flush_needed(struct ufs_hba *hba)
 {
 	int ret;
@@ -9343,7 +9307,9 @@ reinit:
 	/* set the state as operational after switching to desired gear */
 	hba->ufshcd_state = UFSHCD_STATE_OPERATIONAL;
 
+#if 0
 	ufshcd_wb_config(hba);
+#endif
 
 	/*
 	 * If we are in error handling context or in power management callbacks
@@ -10724,9 +10690,13 @@ static int ufshcd_suspend(struct ufs_hba *hba, enum ufs_pm_op pm_op)
 			/* make sure that auto bkops is disabled */
 			ufshcd_disable_auto_bkops(hba);
 		}
+#if 0
 		ufshcd_wb_toggle_flush(hba);
+#endif
 	} else if (!ufshcd_is_runtime_pm(pm_op)) {
+#if 0
 		ufshcd_wb_buf_flush_disable(hba);
+#endif
 		hba->dev_info.keep_vcc_on = false;
 	}
 
@@ -10928,7 +10898,9 @@ static int ufshcd_resume(struct ufs_hba *hba, enum ufs_pm_op pm_op)
 			hba->hibern8_on_idle.state = HIBERN8_EXITED;
 	}
 
+#if 0
 	ufshcd_wb_buf_flush_disable(hba);
+#endif
 	if (!ufshcd_is_ufs_dev_active(hba)) {
 		ret = ufshcd_set_dev_pwr_mode(hba, UFS_ACTIVE_PWR_MODE);
 		if (ret)
